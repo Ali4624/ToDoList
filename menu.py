e96efdaafd2af89bef065411059cb649b21a09ac
Alex <abdullayevalibek4624@gmail.com>
2026-04-10 23:30:32 +0500

    Start point v0.0.1

---
+++ menu.py
@@ -0,0 +1,21 @@
+import time as t
+def Menu():
+    while True:
+        print("==========\n1. Login\n==========\n2. Registration\n==========\n3. Exit\n==========")
+        choice = input("\nEnter your choice:")
+        try:
+            if int(choice)==3:
+                cycles = 3
+                print("Exitting")
+                while cycles >0:
+                    for i in range(1,4):
+                        print('.'*i+" "*(3-i), end = '\r', flush = True)
+                        t.sleep(0.7)
+                    cycles-=1
+                break
+            elif int(choice)==2:
+                pass
+            elif int(choice)==1:
+                pass
+        except ValueError:
+            print("Enter an integer value please!")
