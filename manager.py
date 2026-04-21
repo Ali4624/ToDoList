d3a6383f74fee3730ca0d0bee22a7978aa5306b6
Alex <abdullayevalibek4624@gmail.com>
2026-04-21 11:18:53 +0500

    Added a few features

--- manager.py
+++ manager.py
@@ -3,8 +3,35 @@ import datetime as dt
 import os 
 import json
 counter = 1
-def create_new(username,plan):
+def delete_item(username):
+    if os.path.exists(f"{username}.json"):
+        data = print_all(username)
+        while True:
+            try:    
+                choice = input("\nEnter the number of the plan you want to delete:")
+                deleted_value = data.pop(int(choice))
+                print(f"\n{deleted_value} is deleted from your list!!!\n")
+                break
+            except (ValueError, KeyError):
+                print(f"\nInvalid input!\nPlease enter a number between 1 and {len(data.keys())}")
+    else:
+        print("\nError!\nYou have no active plans now...\nCreate one first please<3\n")
+def print_all(username):
+    if os.path.exists(f"{username}.json"):
+        with open(f"{username}.json", "r", encoding="utf8") as read:
+            data = json.load(read)
+            counter = 1
+            while counter <= len(data.keys()):
+                print("="*10,f"\n{counter}.", end="")
+                for element in data[str(counter)]:
+                    print(f"{element}\n",'-'*10)
+                counter += 1
+        return data
+    else:
+        print("\nError!\nYou have no active plans now...\nCreate one first please<3\n")
+def create_new(username):
     global counter
+    plan = input("\nEnter you plan here\n>>>")
     due_time = input("Format: YYYY.mm.dd: 2025.01.15\nEnter the due date:")
     if os.path.exists(f"{username}.json"):
         with open(f"{username}.json", 'r', encoding='utf8') as files:
@@ -16,23 +43,16 @@ def create_new(username,plan):
             json.dump(data, save, indent = 4, ensure_ascii=False)
     counter += 1
         
-def status_change(username, status = "Completed"):
+def status_change(username, status = "Created"):
     if os.path.exists(f"{username}.json"):
-        with open(f"{username}.json", "r", encoding="utf8") as read:
-            data = json.load(read)
-            counter = 1
-            while counter <= len(data.keys()):
-                print("="*10,f"\n{counter}.", end="")
-                for element in data[str(counter)]:
-                    print(f"{element}\n",'-'*10)
-                counter += 1
-            while True:
-                try:
-                    choice = input("Enter the number of the plan you want to change the status of:")
-                    data[choice][3] = status
-                    break
-                except (ValueError,KeyError):
-                    print(f"\nInvalid input\nTry to enter an integer please...\nFrom 1 to {counter}<3")
+        data = print_all(username)
+        while True:
+            try:
+                choice = input("Enter the number of the plan you want to change the status of:")
+                data[choice][3] = status
+                break
+            except (ValueError,KeyError):
+                print(f"\nInvalid input\nTry to enter an integer please...\nFrom 1 to {counter}<3")
         with open(f"{username}.json", 'w', encoding="utf8") as write:
             json.dump(data, write, indent=4, ensure_ascii=False)
     else:
@@ -47,22 +67,21 @@ def main_menu(username):
         print("3.Remove the list")
         print("4.Display all my plans")
         print("5.Log out...")
-        choice = input("\nEnter a digit(1-4) please\nEnter your choice:")
+        choice = input("\nEnter a digit(1-5) please\nEnter your choice:")
         try: 
             if int(choice) == 1:
-                plan = input("\nEnter you plans here\n>>>")
-                create_new(__username,plan)
+                create_new(__username)
             elif int(choice) == 2:
                 status_change(__username)
             elif int(choice) == 3:
-                pass
+                delete_item(__username)
             elif int(choice) == 4:
-                pass
+                print_all(__username)
             elif int(choice) == 5:
                 break
             else:
                 print("\nYou have entered a wrong choice!\nPlease try again...")
                 continue
         except ValueError:
-            print("Invalid input!\nPlease enter a digit from 1 to 4...")
+            print("Invalid input!\nPlease enter a digit from 1 to 5...")
             continue
